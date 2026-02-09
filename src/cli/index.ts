cb7cb470d9c9eb13529bf9b2ad96db7298b83604
seungyeop-lee <xbeast7@gmail.com>
2026-02-09 21:27:45 +0900

    feat: CLI에 버전 출력 옵션 추가

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,9 +3,11 @@ import { Command } from 'commander';
 import { registerConvertCommand } from './convert-command.ts';
 import { registerCrawlCommand } from './crawl-command.ts';
 import { registerBatchCommand } from './batch-command.ts';
+import packageJson from '../../package.json';
 
 const program = new Command()
   .name('wtm')
+  .version(packageJson.version, '-v, --version')
   .description('웹 사이트를 Markdown으로 변환')
   .showHelpAfterError()
   .enablePositionalOptions();
